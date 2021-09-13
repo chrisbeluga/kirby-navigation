18c3b6bdd50b722b3906a1a122358d4bda53850b
Ahmet Bora <byybora@gmail.com>
2021-09-13 19:57:43 +0300

    Add turkish translations

---
+++ languages/tr.php
@@ -0,0 +1,19 @@
+<?php
+
+	return [
+		'display.empty.text' => 'Henüz menü öğesi yok',
+		'menu.link.title' => 'Sayfa Bağlantısı',
+		'menu.link.text' => 'Menüye bir sayfa ekler',
+		'menu.custom.title' => 'Özel Bağlantı',
+		'menu.custom.text' => 'Menüye harici url\'ler vb. için yararlı olan özel bir bağlantı ekler',
+		'editor.label.text' => 'Metin',
+		'editor.label.title' => 'Başlık',
+		'editor.label.popup' => 'Yeni pencere',
+		'editor.label.close' => 'Kapat',
+		'editor.label.edit' => 'Düzenle',
+		'editor.label.remove' => 'Kaldır',
+		'modal.link.title' => 'Sayfa Ekle',
+		'modal.link.breadcrumb' => 'Geri',
+		'modal.custom.title' => 'Özel Bağlantı Ekle',
+		'help.depth.text' => 'İzin verilen maksimum derinlik:',
+	];
