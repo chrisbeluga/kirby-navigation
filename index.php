a4156ac4619c0a5b65c26b8e3ae963ff808e611f
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2022-04-22 19:25:43 +0200

    Adding support for french langage

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	        'en' => require_once __DIR__ . '/languages/en.php',
 	        'de' => require_once __DIR__ . '/languages/de.php',
 	        'tr' => require_once __DIR__ . '/languages/tr.php',
+	        'fr' => require_once __DIR__ . '/languages/fr.php',
 	    ],
 		'snippets' => [
 			'navigation' => __DIR__ . '/snippets/navigation.php'
---
+++ languages/fr.php
@@ -0,0 +1,23 @@
+<?php
+
+	return [
+		'menu.link.add' => 'Ajouter',
+		'menu.link.title' => 'Lien Kirby',
+		'menu.link.text' => 'Ajouter une page Kirby à ce menu',
+		'menu.custom.title' => 'Lien personnalisé',
+		'menu.custom.text' => 'Ajouter un lien personnalisé à ce menu, utile pour les urls externes etc',
+		'editor.label.id' => 'id',
+		'editor.label.url' => 'url',
+		'editor.label.text' => 'Texte',
+		'editor.label.title' => 'Titre',
+		'editor.label.popup' => 'Popup',
+		'editor.menu.close' => 'Fermer l\'élement',
+		'editor.menu.edit' => 'Éditer l\'élement',
+		'editor.menu.remove' => 'Supprimer l\'élement',
+		'editor.menu.duplicate' => 'Dupliquer l\'élement',
+		'modal.link.title' => 'Ajouter des pages',
+		'modal.link.breadcrumb' => 'Retour',
+		'modal.custom.title' => 'Ajouter un lien personnalisé',
+		'help.depth.text' => 'Profondeur maximale autorisée :',
+		'help.empty.text' => 'Aucun élement de menu pour l\'instant'
+	];
